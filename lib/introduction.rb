c2c9b9242f74bf9d703494ec667d85e0baf02aaf
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-20 00:57:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ def introduction_with_language_optional(name = Edwin, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional (name, "Python")
+introduction_with_language_optional (name, Python)
 
 
 
