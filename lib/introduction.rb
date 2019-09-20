5c9bce26986749c6c8c665e56c4445f88c7dfbe1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-20 00:49:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language(name = Dan, language = Ember.js)
 end
 
 def introduction_with_language_optional(name = Edwin, language = "Python")
-  puts "Hi, my name is #{name} and I am learning to program in {language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
